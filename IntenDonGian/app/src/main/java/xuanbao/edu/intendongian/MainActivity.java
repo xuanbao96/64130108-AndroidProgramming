c3e3b4de56dc61ad0177bc201b8d549d7cdb27cd
xuanbao96 <baod7932@gmail.com>
2025-03-06 14:39:29 +0700

    IntentDonGian
    Gắn bộ lắng nghe xử lý sự kiện và tạo intent 2 tham số (1 là màn hình hiện tại, 2 là màn hình chuyển tới).

--- IntenDonGian/app/src/main/java/xuanbao/edu/intendongian/MainActivity.java
+++ IntenDonGian/app/src/main/java/xuanbao/edu/intendongian/MainActivity.java
@@ -1,6 +1,8 @@
 package xuanbao.edu.intendongian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -23,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Tìm điều khiển nút bấm
         TimDieuKhien();
+        //Gắn bộ lắng nghe sự kiện
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Xử lý chuyển màn hình
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
