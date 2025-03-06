e05856fc48d30de384e8e1e3195548a8aa2f6e35
xuanbao96 <baod7932@gmail.com>
2025-03-06 16:18:09 +0700

    IntentLogin
    Thêm sự kiện chuyển hướng ở mainactivity.

--- IntentLogin/app/src/main/java/xuanbao/edu/intentlogin/MainActivity.java
+++ IntentLogin/app/src/main/java/xuanbao/edu/intentlogin/MainActivity.java
@@ -1,6 +1,9 @@
 package xuanbao.edu.intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //Tìm đk
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        //Sự kiện
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
