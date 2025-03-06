09b00d2381e0d84b9c45759853950c7de878bee1
xuanbao96 <baod7932@gmail.com>
2025-03-06 16:35:50 +0700

    IntentLogin
    Xử lý việc nhận tên  và khẩu bằng việc nhận dữ liệu và gửi đi

--- IntentLogin/app/src/main/java/xuanbao/edu/intentlogin/LoginActivity.java
+++ IntentLogin/app/src/main/java/xuanbao/edu/intentlogin/LoginActivity.java
@@ -1,6 +1,11 @@
 package xuanbao.edu.intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        Button btnXacNhan = (Button) findViewById(R.id.btnOK);
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText edTenDN = (EditText) findViewById(R.id.edtUsername);
+                EditText edPass = (EditText) findViewById(R.id.edtPass);
+                String tenDangNhap = edTenDN.getText().toString();
+                String mk = edPass.getText().toString();
+                //Kiểm tra mật khẩu
+                if (tenDangNhap.equals("dinhxuanbao") && mk.equals("123"))//mk đúng
+                {//chuyển sang màn hình home
+                    Intent iQuiz = new Intent(LoginActivity.this, HomeActivity.class);
+                    //Gọi dữ liệu vào iQuiz, dạng key - value; key được dùng để bên kia lọc ra dữ liệu
+                    iQuiz.putExtra("ten_dang_nhap",tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap",mk);
+                    //Gửi đi
+                    startActivity(iQuiz);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "BẠN NHẬP SAI THÔNG TIN",Toast.LENGTH_LONG)
+                }
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
