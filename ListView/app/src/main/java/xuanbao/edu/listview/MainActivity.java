0bae33ac38eccbd6ed3aeb7c08fa6fd4e2d355cf
xuanbao96 <baod7932@gmail.com>
2025-03-11 13:49:21 +0700

    ListView
    Thêm bước chuẩn bị dữ liệu

--- ListView/app/src/main/java/xuanbao/edu/listview/MainActivity.java
+++ ListView/app/src/main/java/xuanbao/edu/listview/MainActivity.java
@@ -1,6 +1,7 @@
 package xuanbao.edu.listview;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ListView listViewNNLT;
+    ArrayList<String> dsNgonNguLT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listViewNNLT = findViewById(R.id.lvLapTrinh);
+        dsNgonNguLT = new ArrayList<String>();
+        dsNgonNguLT.add("Python");
+        dsNgonNguLT.add("PHP");
     }
 }
