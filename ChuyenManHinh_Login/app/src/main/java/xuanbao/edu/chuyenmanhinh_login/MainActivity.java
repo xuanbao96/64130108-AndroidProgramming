01946d552f0f71d34aeb64221d1515386c5f831f
xuanbao96 <baod7932@gmail.com>
2025-03-11 15:00:22 +0700

    ListViewBaiHat
    Đưa dữ liệu vào list view

--- ChuyenManHinh_Login/app/src/main/java/xuanbao/edu/chuyenmanhinh_login/MainActivity.java
+++ ChuyenManHinh_Login/app/src/main/java/xuanbao/edu/chuyenmanhinh_login/MainActivity.java
@@ -1,4 +1,4 @@
-package xuanbao.edu.listbaihatyeuthich;
+package xuanbao.edu.chuyenmanhinh_login;
 
 import android.os.Bundle;
 
@@ -8,8 +8,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,14 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        // Hiển thị dữ liệu lên ListView
-        //B1: Chuẩn bị nguồn dữ liệu (có thể tạo sẵn(hardcode), lấy từ tệp, csdl
-        ArrayList<String> nguonDuLieu = new ArrayList<String>();
-        nguonDuLieu.add("Miền đất hứa");
-        nguonDuLieu.add("Đừng xấu hổ");
-        nguonDuLieu.add("Tiến vào màn đêm");
-        nguonDuLieu.add("Tái sinh");
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
