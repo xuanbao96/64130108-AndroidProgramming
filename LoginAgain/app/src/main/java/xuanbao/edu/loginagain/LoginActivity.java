02a03a9f9177fcba71ca86f0a0d6732a3631f4e0
xuanbao96 <baod7932@gmail.com>
2025-03-13 16:51:07 +0700

    LoginAgain
    Gắn điều kiện if dành cho việc xác nhận đúng thông tin được chỉ định không thì mới được đăng nhập

--- LoginAgain/app/src/main/java/xuanbao/edu/loginagain/LoginActivity.java
+++ LoginAgain/app/src/main/java/xuanbao/edu/loginagain/LoginActivity.java
@@ -1,9 +1,11 @@
 package xuanbao.edu.loginagain;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,6 +28,16 @@ public class LoginActivity extends AppCompatActivity {
                 EditText edPass = (EditText) findViewById(R.id.edtPass);
                 String tenDangNhap = edTenDN.getText().toString();
                 String mk = edPass.getText().toString();
+                
+                if (tenDangNhap.equals("xuanbao") && mk.equals("123")){
+                    Intent iQuiz = new Intent(LoginActivity.this,HomeActivity.class);
+                    iQuiz.putExtra("ten_dang_nhap",tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap",mk);
+                    startActivity(iQuiz);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"THÔNG TIN ĐÃ NHẬP SAI",Toast.LENGTH_LONG);
+                }
             }
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
