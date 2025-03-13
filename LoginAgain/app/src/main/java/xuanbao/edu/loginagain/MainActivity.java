e34b2d1c37b12b68771b04cd553a41686ee9f330
xuanbao96 <baod7932@gmail.com>
2025-03-13 16:37:15 +0700

    AgainLogin
    Đã ánh xạ với nút đăng nhập và gắn bộ lắng nghe cho việc đăng nhập.

--- LoginAgain/app/src/main/java/xuanbao/edu/loginagain/MainActivity.java
+++ LoginAgain/app/src/main/java/xuanbao/edu/loginagain/MainActivity.java
@@ -1,6 +1,9 @@
 package xuanbao.edu.loginagain;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(iLogin);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
