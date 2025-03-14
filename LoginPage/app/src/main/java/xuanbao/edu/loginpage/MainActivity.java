95f523cb4ddab54fc4f9c4ca25722cfb8f08c1d6
xuanbao96 <baod7932@gmail.com>
2025-03-14 15:45:44 +0700

    LoginPage
    Khai báo dữ liệu và ánh xạ đến từng thành phần

--- LoginPage/app/src/main/java/xuanbao/edu/loginpage/MainActivity.java
+++ LoginPage/app/src/main/java/xuanbao/edu/loginpage/MainActivity.java
@@ -1,6 +1,8 @@
 package xuanbao.edu.loginpage;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.passwword);
+        loginButton = findViewById(R.id.LoginButton);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
