947c9f67b28960918f279f36b882d1e4a6953afb
xuanbao96 <baod7932@gmail.com>
2025-03-18 13:41:12 +0700

    VdChao
    Bắt sự kiện cho button

--- ChaoABC/app/src/main/java/xuanbao/edu/chaoabc/MainActivity.java
+++ ChaoABC/app/src/main/java/xuanbao/edu/chaoabc/MainActivity.java
@@ -26,7 +26,17 @@ public class MainActivity extends AppCompatActivity {
         edtName = findViewById(R.id.edtName);
         btnChao = findViewById(R.id.btnHello);
 
-        
+        btnChao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edtName.getText().toString().trim();
+                if(!name.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Chào"+name,Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"Vui lòng nhập tên"+name,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
