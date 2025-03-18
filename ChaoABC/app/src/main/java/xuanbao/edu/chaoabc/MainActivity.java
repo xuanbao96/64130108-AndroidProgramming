c55eee15d573c42779a2c4e0acee161602681c9d
xuanbao96 <baod7932@gmail.com>
2025-03-18 13:32:30 +0700

    VdChao
    Khai báo và ánh xạ view

--- ChaoABC/app/src/main/java/xuanbao/edu/chaoabc/MainActivity.java
+++ ChaoABC/app/src/main/java/xuanbao/edu/chaoabc/MainActivity.java
@@ -3,6 +3,7 @@ package xuanbao.edu.chaoabc;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,20 +14,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtName;
+    Button btnChao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.btn1);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Chao abc",Toast.LENGTH_LONG).show();
-            }
-        });
+        edtName = findViewById(R.id.edtName);
+        btnChao = findViewById(R.id.btnHello);
+
+        
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
