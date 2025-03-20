6c3fc687132c92793d3ce7e20f049a31496eceb2
xuanbao96 <baod7932@gmail.com>
2025-03-20 15:02:10 +0700

    Congtrunhanchia_LamLai
    Khai báo dữ liệu và ánh xạ dữ liệu tương ứng

--- AddSubMulDiv_Onclick_LamLai/app/src/main/java/xuanbao/edu/addsubmuldiv_onclick_lamlai/MainActivity.java
+++ AddSubMulDiv_Onclick_LamLai/app/src/main/java/xuanbao/edu/addsubmuldiv_onclick_lamlai/MainActivity.java
@@ -1,6 +1,8 @@
 package xuanbao.edu.addsubmuldiv_onclick_lamlai;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong,nutTru,nutNhan,nutChia;
+    Void TimDieuKhien(){
+        editTextSo1 = (EditText) findViewById(R.id.edtSo1);
+        editTextSo2 = (EditText) findViewById(R.id.edtSo2);
+        editTextKQ = (EditText) findViewById(R.id.edtKQ);
+        nutCong = (Button) findViewById(R.id.btnCong);
+        nutTru = (Button) findViewById(R.id.btnTru);
+        nutNhan = (Button) findViewById(R.id.btnNhan);
+        nutChia = (Button) findViewById(R.id.btnChia);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
