be5f9f38e1216d2cf99db75b5b498b3ca403acd3
xuanbao96 <baod7932@gmail.com>
2025-03-20 15:11:03 +0700

    Congtrunhanchia_LamLai
    Triển khai bộ lắng nghe ẩn danh cho từng nút

--- AddSubMulDiv_Onclick_LamLai/app/src/main/java/xuanbao/edu/addsubmuldiv_onclick_lamlai/MainActivity.java
+++ AddSubMulDiv_Onclick_LamLai/app/src/main/java/xuanbao/edu/addsubmuldiv_onclick_lamlai/MainActivity.java
@@ -1,8 +1,10 @@
 package xuanbao.edu.addsubmuldiv_onclick_lamlai;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextSo2;
     EditText editTextKQ;
     Button nutCong,nutTru,nutNhan,nutChia;
-    Void TimDieuKhien(){
+    void TimDieuKhien(){
         editTextSo1 = (EditText) findViewById(R.id.edtSo1);
         editTextSo2 = (EditText) findViewById(R.id.edtSo2);
         editTextKQ = (EditText) findViewById(R.id.edtKQ);
@@ -30,10 +32,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
+        nutCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLy_Cong();
+            }
+        });
+        nutTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLy_Tru();
+            }
+        });
+        nutNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLy_Nhan();
+            }
+        });
+        nutChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLy_Chia();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    void XuLy_Cong(){
+
+    }
+    void XuLy_Tru(){
+
+    }
+    void XuLy_Nhan(){
+
+    }
+    void XuLy_Chia(){
+
     }
 }
