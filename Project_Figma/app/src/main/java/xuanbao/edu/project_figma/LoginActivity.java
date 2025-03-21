cea4a40a25b693f7e8170c734c6c0ba6522d22d3
xuanbao96 <baod7932@gmail.com>
2025-03-21 22:14:25 +0700

    Project_Figma
    Khai báo dữ liệu ánh xạ với đăng nhập qua sự kiện chuyển màn hình từ loginactivity đã nhận dữ liệu đăng nhập thì sẽ gửi tín hiệu để chuyển sang màn hình home

--- Project_Figma/app/src/main/java/xuanbao/edu/project_figma/LoginActivity.java
+++ Project_Figma/app/src/main/java/xuanbao/edu/project_figma/LoginActivity.java
@@ -1,6 +1,11 @@
 package xuanbao.edu.project_figma;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,27 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button) findViewById(R.id.loginButton);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edtTenDN = (EditText) findViewById(R.id.username);
+                EditText edtPass = (EditText) findViewById(R.id.password);
+                String tenDangNhap = edtTenDN.getText().toString();
+                String mk = edtPass.getText().toString();
+
+                if(tenDangNhap.equals("dinhxuanbao") && mk.equals("123")){
+                    Intent iSkip = new Intent(LoginActivity.this,HomeActivity.class);
+                    iSkip.putExtra("ten_dang_nhap",tenDangNhap);
+                    iSkip.putExtra("mk_dang_nhap",mk);
+                    startActivity(iSkip);
+                }else{
+                    Toast.makeText(LoginActivity.this,"BẠN NHẬP SAI THÔNG TIN",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
