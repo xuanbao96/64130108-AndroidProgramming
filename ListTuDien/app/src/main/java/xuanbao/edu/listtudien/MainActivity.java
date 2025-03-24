9a7cc98f2337421626702b82bb4e021191a34247
xuanbao96 <baod7932@gmail.com>
2025-03-24 10:36:25 +0700

    ListIndex
    Đưa dữ liệu vào, sử dụng danh sách mảng (ArrayList) để add các dữ liệu mình chọn (có thể từ csdl, database hoặc tự mình nghĩ ra)

--- ListTuDien/app/src/main/java/xuanbao/edu/listtudien/MainActivity.java
+++ ListTuDien/app/src/main/java/xuanbao/edu/listtudien/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> nguonDL = new ArrayList<String>();
+        nguonDL.add("Tiếng Anh");
+        nguonDL.add("Tiếng Việt");
+        nguonDL.add("Tiếng Nhật");
+        nguonDL.add("Tiếng Nga");
+        nguonDL.add("Tiếng Pháp");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
