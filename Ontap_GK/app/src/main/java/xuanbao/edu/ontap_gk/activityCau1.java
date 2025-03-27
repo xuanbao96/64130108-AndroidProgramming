217d5f27246c3cd5b301454cabb89a11070ff6cc
xuanbao96 <baod7932@gmail.com>
2025-03-27 13:21:42 +0700

    Ontap_GK
    Thiết lập tính tổng cho activityCau1.java

--- Ontap_GK/app/src/main/java/xuanbao/edu/ontap_gk/activityCau1.java
+++ Ontap_GK/app/src/main/java/xuanbao/edu/ontap_gk/activityCau1.java
@@ -1,6 +1,9 @@
 package xuanbao.edu.ontap_gk;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,42 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class activityCau1 extends AppCompatActivity {
-
+    EditText editText1;
+    EditText editText2;
+    EditText editTextKQ;
+    Button buttonCong;
+    void TimDK(){
+        editText1 = (EditText) findViewById(R.id.edtA);
+        editText2 = (EditText) findViewById(R.id.edtB);
+        editTextKQ = (EditText) findViewById(R.id.edtKQ);
+        buttonCong = (Button) findViewById(R.id.btnCong);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cau1);
+        TimDK();
+        buttonCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PhepCong();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    void PhepCong(){
+        String so1 = editText1.getText().toString();
+        String so2 = editText2.getText().toString();
+        float n1 = Float.parseFloat(so1);
+        float n2 = Float.parseFloat(so2);
+        float sum = n1 + n2;
+        String kq = String.valueOf(sum);
+        editTextKQ.setText(kq);
     }
 }
