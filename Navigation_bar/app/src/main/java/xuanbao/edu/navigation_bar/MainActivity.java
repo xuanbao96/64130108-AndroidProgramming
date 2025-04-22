4fa2c1dfdd88fdfe79752c9381d0bd675694cdf2
xuanbao96 <baod7932@gmail.com>
2025-04-22 14:25:52 +0700

    Navigation_Bar
    Code xử lý lắng nghe và bắt sự kiện để duyệt điều kiện để đưa thông báo để hiển thị lên màn hình

--- Navigation_bar/app/src/main/java/xuanbao/edu/navigation_bar/MainActivity.java
+++ Navigation_bar/app/src/main/java/xuanbao/edu/navigation_bar/MainActivity.java
@@ -1,24 +1,40 @@
 package xuanbao.edu.navigation_bar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNav = findViewById(R.id.bot_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int mnuItemDuocChonID = item.getItemId();
+                if (mnuItemDuocChonID == R.id.mnu_home) {
+                    // Thay fragment home
+                    Toast.makeText(MainActivity.this, "Thay HOME", Toast.LENGTH_LONG).show();
+                }else if (mnuItemDuocChonID == R.id.mnu_search) {
+                    Toast.makeText(MainActivity.this, "Thay SEARCH", Toast.LENGTH_LONG).show();
+                }else if (mnuItemDuocChonID == R.id.mnu_profile) {
+                    Toast.makeText(MainActivity.this, "Thay PROFILE", Toast.LENGTH_LONG).show();
+                }else return false;
+                return true;
+            }
         });
     }
 }
--- README.md
+++ README.md
@@ -3178,4 +3178,44 @@ public class FooterFragment extends Fragment {
         app:layout_constraintStart_toStartOf="parent"
         app:layout_constraintTop_toTopOf="parent" />
 </androidx.constraintlayout.widget.ConstraintLayout>
+-Code xử lý lắng nghe và bắt sự kiện để duyệt điều kiện để đưa thông báo để hiển thị lên màn hình
+package xuanbao.edu.navigation_bar;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bot_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int mnuItemDuocChonID = item.getItemId();
+                if (mnuItemDuocChonID == R.id.mnu_home) {
+                    // Thay fragment home
+                    Toast.makeText(MainActivity.this, "Thay HOME", Toast.LENGTH_LONG).show();
+                }else if (mnuItemDuocChonID == R.id.mnu_search) {
+                    Toast.makeText(MainActivity.this, "Thay SEARCH", Toast.LENGTH_LONG).show();
+                }else if (mnuItemDuocChonID == R.id.mnu_profile) {
+                    Toast.makeText(MainActivity.this, "Thay PROFILE", Toast.LENGTH_LONG).show();
+                }else return false;
+                return true;
+            }
+        });
+    }
+}
